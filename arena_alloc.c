d42f727e35a81dd3e9288f5ad22de3af2cc5d47e
Ashvin Sivakolunthu <asivakolunthu@outlook.com>
2026-09-23 17:16:35 -0400

    Improved note taking on both added files

--- arena_alloc.c
+++ arena_alloc.c
@@ -1,62 +1,63 @@
 #include "base.h"
-/*Notes:
- * Metadata: hidden feature after malloc that provides extra information about the memory space you've borrowed. Enables to track the size of each allocation, efficiently free memory, merge free chunks, and maintain internal data structures. Mainly stores prev_size, curr_size, fd & bk (only used when chunk is free)
- *
- *
- * Contiguous memorry allocation: storing a consistent, non-broken memory sequence in a process, leaving the other contiguous memory in the same place. It can be easily divided by equally partitions of the same, fixed size.
- *
- * */
+/*
+Notes:
+ > Metadata: hidden feature after malloc that provides extra information about the memory space you've borrowed. 
+  Enables to track the size of each allocation, efficiently free memory, merge free chunks, and maintain 
+  internal data structures. Mainly stores prev_size, curr_size, fd & bk (only used when chunk is free)
+ > Contiguous memorry allocation: storing a consistent, non-broken memory sequence in a process, leaving the other 
+  oontiguous memory in the same place. It can be easily divided by equally partitions of the same, fixed size.
+*/
 
 
-//The user should know its data type's number of bytes
+// capacity: as the name suggests, the function takes a 8-byte capacity size to borrow memory space in the virtual memory (VM)
 Arena* create_arena(u64 capacity) {
-    // Can request memory space on either virtual address space (recommended) or malloc for each arena
-    // Can request by multiplying capacity by 1 byte, typecast Arena* with capacity space
-    Arena* arena = (Arena*)malloc(capacity); 
 
+    Arena* arena = (Arena*)malloc(capacity); 
     assert(arena);
     
+    /* 
+     offset has to start at the arena's BASE value because 
+     the memory space malloced has to reserve the size of Arena type
+     included.
+    */
     arena->offset = BASE;   
     arena->capacity = capacity;
     
     return arena;
 }
-
+// takes the arena to free the memory space
 void destroy_arena(Arena* arena) {
     free(arena); 
     arena = NULL;
 }
 
-// size takes the sizeof() from a data or reference type 
+// size: takes the sizeof() from a data type or reference type 
 void* arena_alloc(Arena* arena, u64 size) {
-    
-    u64 align_pos = ALIGN_UP(arena->offset, PTR_SIZE); // Make sure the position is in alignmenot, otherwise we would access values in fragmented space
-    // Make sure we add by alignment along with correct padding to avoid fragmentation and non-contiguous uneven space
-    assert(align_pos + size < arena->capacity); 
-    arena->offset = align_pos + size; 
-    
+    /* 
+      > Make sure the position is in alignment, otherwise we would access values in fragmented space
+      > Make sure we add by alignment along with correct padding to avoid fragmentation and
+        non-contiguous uneven space
+     */
+    u64 align_pos = ALIGN_UP(arena->offset, ALIGN);  //get aligned position     
+    assert(align_pos + size < arena->capacity); //make sure the new position (align_pos + size) is not greater than the capacity 
+    arena->offset = align_pos + size; // set new postion to offset
     u8* out = (u8*)arena + align_pos; // increment by 1 byte, why?
    
     return out;
 }
 
+// takes the specific arena to pop the top object by taking its size of object type (prev_size)
 void* arena_pop(Arena* arena, u64 prev_size) {
    
     assert(prev_size < arena->capacity && prev_size < arena->offset); 
-    u64 align_pos = ALIGN_UP(prev_size, PTR_SIZE); //alignment in memory space is very necessary to ensure contiguous sequence 
+    u64 align_pos = ALIGN_UP(prev_size, ALIGN); //alignment in memory space is very necessary to ensure contiguous sequence 
     arena->offset -= align_pos;
 
     
     return arena; 
 }
 
+// takes the specific arena to clear all objects 
 void arena_clear(Arena* arena)  {
     arena->offset = BASE; 
 }
-
-
-
-
-
-
-
--- base.h
+++ base.h
@@ -6,50 +6,63 @@
 #include <stdbool.h>
 #include <assert.h>
 
-// Shifts n to the left to add 30 zeros
-// GiB to GB: GB = GiB * (1024^3/1000^3) 
-#define PTR_SIZE (sizeof(void*))
-// Since we are returning the capacity size including the size of u64 twice 
+/*
+  Used for one of the agruments in ALIGN_UP to maintain contiguous sequence of objects in memory.
+  The size of the void pointer is always 8 bytes.
+*/
+#define ALIGN (sizeof(void*))
+/*
+ Since we are returning the capacity size including the size of u64 twice, 
+ which stores the value of offset and capacity in the memory space
+*/
 #define BASE (sizeof(Arena))  
-// Rounding cursor to the next aligned address to ensure we add space contiguously
-#define ALIGN_UP(curr, pow) (((u64)curr + (u64)pow - 1) & ~((u64)pow - 1))
+/* 
+ This function macro rounds the cursor to the next aligned address to ensure the
+ memory space is contiguous. 'o' is the current offset, while 'p' stand for the power of 2 (1,2,4,8,etc). 
+*/
+#define ALIGN_UP(o, p) (((u64)o + (u64)p - 1) & ~((u64)p - 1))
 
+// typedef macros to type faster and easier (shoutout to MacigcalBat!)
 typedef int8_t i8; 
 typedef int16_t i16; 
 typedef int32_t i32; 
 typedef int64_t i64; 
-
 typedef uint8_t u8; 
 typedef uint16_t u16;
 typedef uint32_t u32; 
 typedef uint64_t u64; 
 
+// Some defined macros that convert from bytes to larger metric bytes
 #define MiB(n) ((u64)n << 10)
 #define KiB(n) ((u64)n << 20)
-#define GiB(n) ((u64)(n) << 30)
+#define GiB(n) ((u64)n << 30)
 
+/* 
+ An arena requires an offset to track the position or displacement. 
+ It also requires a capacity to maintain the size of the arena consistently 
+*/
 typedef struct arena {
     u64 offset;
     u64 capacity;
 } Arena; 
 
-// Could commit or reserve space in arena using virtual address space
-// Consider position alignment and fragmentation. 
+// Creates an arena allocator by accepting a size, which is the capacity of the chunk of memory
 Arena* create_arena(u64);
+/*
+ Destroys the arena simply by freeing it. It only takes the 
+ Arena* type, which is simply the arena that was created.
+*/
 void destroy_arena(Arena*);
-// namely arena_alloc. Can add as many object pointers as we can by definiting the correct sizeof into the function.Return pointer, and can be of any type by typecasting for init object structs or opther data types, as long as we allocate the right byte size, and ensure alignment to properly store size into arena. 
+
+/* 
+ Adds as many object pointers as I can by definiting the correct sizeof into the function.
+ Returns pointer, and can be of any type by typecasting for object structs or other 
+ data types, as long as we allocate the right byte size, and ensure alignment 
+ to properly store size into arena. 
+*/
 void* arena_alloc(Arena*, u64); 
-
+// Pops an object in the arena from the top, acting like a stack. It needs to know its previous size of data/reference type.
 void* arena_pop(Arena*, u64); 
+// Completely clears the specified arena.
 void arena_clear(Arena*);
-
-
-/* References:
- * > https://www.dgtlgrove.com/p/untangling-lifetimes-the-arena-allocator
- * > https://github.com/CobbCoding1/c-allocators
- * > https://andreleite.com/posts/2025/nstl/virtual-memory-arena-allocator/
- * > https://www.youtube.com/watch?v=jgiMagdjA1s
- *
- * */ 
-
 
